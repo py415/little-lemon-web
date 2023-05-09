e357ce5913218d2574cfa02da3e8a4934d0af64e
py415 <philipyu1997@gmail.com>
2023-05-09 10:37:56 -0700

    Add meta tags and setup OGP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
+  // State
+  const ogImageUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/Logo.svg`;
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Little Lemon is a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist."
+        />
+        <meta name="og:title" content="Little Lemon" />
+        <meta
+          name="og:description"
+          content="We are family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist."
+        />
+        <meta name="og:image" content={ogImageUrl} />
+      </Head>
       <body>
         <Main />
         <NextScript />
