c752d245248b6450035df6867da6effdbbd9ec1b
py415 <philipyu1997@gmail.com>
2023-07-02 22:38:21 -0700

    Update API url with environment variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { NavItem } from "@/components/navigation-item/navigation-item";
 export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000"
-    : "https://little-lemon-server.vercel.app";
+    : process.env.API_URL;
 export const LOGIN_API = `${API_URL}/api/token/`;
 export const BOOKING_API = `${API_URL}/api/bookings/`;
 export const NAV_ITEMS: NavItem[] = [
